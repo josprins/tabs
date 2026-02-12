4d207a4365959f509624bbe7e1dc76e6996ba9fc
Jos <j.prins@performation.com>
2026-02-12 17:22:37 +0100

    Initial setup & tabs logic roughly the way I want it

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomePage from '@/views/HomePage.vue'
-import DetailsPage from '@/views/DetailsPage.vue'
-import AssortmentPage from '@/views/AssortmentPage.vue'
-import QuestionnairesPage from '@/views/QuestionnairesPage.vue'
+import HomePage from '../views/HomePage.vue'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,11 +7,6 @@ const router = createRouter({
 		{
 			path: '/',
 			component: HomePage,
-			children: [
-				{ path: 'details', name: 'details', component: DetailsPage },
-				{ path: 'assortment', name: 'assortment', component: AssortmentPage },
-				{ path: 'questionnaires', name: 'questionnaires', component: QuestionnairesPage },
-			],
 		},
 	],
 })
